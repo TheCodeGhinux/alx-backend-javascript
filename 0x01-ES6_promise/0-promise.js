49e76cc669598fa3ea85389bf8de593b55ebd4b4
Ayomide <ilesanmibry@gmail.com>
2024-06-26 05:35:29 +0100

    first commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true
+
+    if (success) {
+      resolve('Success response')
+    } else {
+      reject(new Error('Failure response'))
+    }
+  })
+}
